972a10af11d02f2d868d4004639842b36af512f6
Steven Lee <Steven@Blueberry.local>
2018-05-24 15:14:41 +0100

    Pass 'should confirm that when a function is attached to an object, 'this' refers to the object' Test

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function() {
       }
     };
    
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(meglomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2018);
+    expect(meglomaniac.calculateAge()).toBe(2018-1970);
   });
 
   describe("'in' keyword", function() {
